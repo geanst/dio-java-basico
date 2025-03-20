b2201f517a01413af79fc2a42c0e6e0ae3b57b53
geanst <stein.gean@gmail.com>
2025-03-20 17:43:04 -0300

    Desafio Controle Fluxo

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+        super("Parametros inválidos.");
+    }
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
